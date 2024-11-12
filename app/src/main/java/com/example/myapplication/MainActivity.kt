e8bf75823b285bc6f543fab5ac353ddac3ca9c56
mmmaggggs <139990900+mmmaggggs@users.noreply.github.com>
2024-11-11 22:29:31 -0600

    dark mode, formatting change for driver info
    still working on formatting the rest

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.myapplication
 
 
+
+/*
+TO DO:
+        -- change background color to dark
+        -- change sensor displays to lighter colors
+        -- design UI on ipad
+
+ */
+
 import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
@@ -9,6 +18,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,22 +26,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,6 +60,13 @@ import com.github.anastr.speedometer.TubeSpeedometer
 import kotlin.random.Random
 import android.widget.SeekBar
 import androidx.core.content.ContextCompat
+import com.github.anastr.speedometer.components.text.SpeedText
+import kotlinx.collections.immutable.immutableListOf
+import kotlinx.collections.immutable.persistentListOf
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.layout.*
 
 
 class MainActivity : ComponentActivity() {
@@ -52,17 +75,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color(25, 25, 25)) { innerPadding ->
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding)
+                            .background(Color(25, 25, 25))
+                            //.verticalScroll(rememberScrollState())
                     ) {
-                        // TAMU Solar Logo positioned at top left
-
-                        GreetingImage(
+                         //TAMU Solar Logo positioned at top left
+                        Box(
                             modifier = Modifier
-                        )
+                                .fillMaxWidth()
+                                .background(Color(80, 0, 0))
+                                .padding(16.dp)
+                                //.align(Alignment.TopCenter)
+                        ) {
+                            GreetingImage(
+                                modifier = Modifier
+                            )
+                        }
                         val driverInfo = mutableListOf("Average Speed: 0 m/s", "Average Temperature: 0°F")
                         val mainBatteryInfo = mutableListOf("Voltage: 0 V", "Current: 0 A")
                         val supBatteryInfo = mutableListOf("Voltage: 0 V", "Current: 0 A")
@@ -76,6 +108,7 @@ class MainActivity : ComponentActivity() {
                         )
 
                     }
+
                 }
 
             }
@@ -88,73 +121,101 @@ fun LazyGridItemScope.PointerSpeedometer(name: String, data: List<String>, modif
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
+            .padding(16.dp)
         ) {
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = 8.dp)
-                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = name,
-                    fontSize = 16.sp,
-                    color = Color.Black,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
                 )
             }
-        if (name == "Driver Info") {
-            var speed by remember { mutableStateOf(0f) }
-            val currentSpeed by animateFloatAsState(
-                targetValue = speed,
-                animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing)
-            )
-            Text(
-                text = "Average Speed",
-                fontSize = 12.sp
-            )
-            PointerSpeedometer(
-                modifier = Modifier.size(250.dp),
-                speed = currentSpeed,
-            )
-            Button(
-                onClick = {
-                    // Change speed to start the animation
-                    speed = Random.nextFloat() * 100
-                },
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(4.dp) // Reduce padding
-                    .size(120.dp, 40.dp) // Adjust size (width x height)
-            ) {
-                Text("Update speed")
-            }
 
-            // temperature
-            var temperature by remember { mutableStateOf(0f) }
-            val currentTemperature by animateFloatAsState(
-                targetValue = temperature,
-                animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing)
-            )
-            Text(
-                text = "Average Temperature",
-                fontSize = 12.sp
-            )
-            TubeSpeedometer(
-                modifier = Modifier.size(120.dp),
-                speed = currentTemperature,
-                unit = "°F"
-            )
-            Button(
-                onClick = {
-                    // Change speed to start the animation
-                    temperature = Random.nextFloat() * 120 - 10
-                },
+        if (name == "Driver Info") {
+            Box(
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(4.dp) // Reduce padding
-                    .size(120.dp, 40.dp) // Adjust size (width x height)
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                contentAlignment = Alignment.Center
             ) {
-                Text("Update temp")
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly, // Adjusts spacing between items
+                    modifier = Modifier.fillMaxWidth()
+                )  {
+
+                var speed by remember { mutableStateOf(0f) }
+                val currentSpeed by animateFloatAsState(
+                    targetValue = speed,
+                    animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing)
+                )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.weight(1f) // Makes sure this takes up equal space
+                    ) {
+                        Text(
+                            text = "Average Speed",
+                            fontSize = 12.sp,
+                            color = Color.White
+                        )
+                        PointerSpeedometer(
+                            modifier = Modifier.size(150.dp),
+                            speed = currentSpeed,
+                            backgroundCircleColor = Color(80, 0, 0),
+                            barColor = Color(255, 255, 255),
+                            maxSpeed = 75f,
+
+                            unit = "mph"
+                        )
+                        Button(
+                            onClick = {
+                                // Change speed to start the animation
+                                speed = Random.nextFloat() * 100
+                            },
+                        ) {
+                            Text("Random speed")
+                        }
+                    }
+
+                // temperature
+                var temperature by remember { mutableStateOf(0f) }
+                val currentTemperature by animateFloatAsState(
+                    targetValue = temperature,
+                    animationSpec = tween(durationMillis = 2000, easing = FastOutSlowInEasing)
+                )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.weight(1f) // Ensures equal space
+                    ) {
+                        Text(
+                            text = "Average Temperature",
+                            fontSize = 12.sp,
+                            color = Color.White
+                        )
+                        TubeSpeedometer(
+                            modifier = Modifier.size(120.dp),
+                            speed = currentTemperature,
+                            backgroundCircleColor = Color(80, 0, 0),
+                            unit = "°F"
+                        )
+                        Button(
+                            onClick = {
+                                // Change speed to start the animation
+                                temperature = Random.nextFloat() * 120 - 10
+                            },
+                            modifier = Modifier
+                                .padding(4.dp) // Reduce padding
+                                .size(120.dp, 40.dp) // Adjust size (width x height)
+                        ) {
+                            Text("Update temp")
+                        }
+                    }
+                    }
             }
 
         } else if (name == "Motor") {
@@ -165,7 +226,8 @@ fun LazyGridItemScope.PointerSpeedometer(name: String, data: List<String>, modif
                 )
                 Text(
                     text = "Average Temperature",
-                    fontSize = 12.sp
+                    fontSize = 12.sp,
+                    color = Color.White
                 )
                 TubeSpeedometer(
                     modifier = Modifier.size(120.dp),
@@ -186,7 +248,8 @@ fun LazyGridItemScope.PointerSpeedometer(name: String, data: List<String>, modif
                 }
             Text(
                 text = "Current and Power",
-                fontSize = 12.sp
+                fontSize = 12.sp,
+                color = Color.White
             )
         } else {
             data.forEach { metric: String ->
@@ -207,11 +270,66 @@ fun LazyGridItemScope.PointerSpeedometer(name: String, data: List<String>, modif
                     }
                     Text(
                         text = metric,
-                        fontSize = 12.sp
+                        fontSize = 12.sp,
+                        color = Color.White
                     )
                 }
             }
         }
+    }
+}
+
+
+
+@Composable
+fun DataGrid(
+    modifier : Modifier = Modifier,
+    data : Map<String, List<String>>
+) {
+    // columns = GridCells.Adaptive(dp size of each cell) or GridCells.Fixed(fixed number of cells)
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(1),
+    ) {
+        items(data.entries.size) { d ->
+            val entry = data.entries.elementAt(d)
+            PointerSpeedometer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                name = entry.key,
+                data = entry.value
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+
+    }
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.official_small)
+    Row(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.size(100.dp)
+        )
+        Text(
+            text = "TAMU Solar Car",
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(10.dp),
+            color = Color.White,
+            fontSize = 25.sp
+        )
     }
 }
 
